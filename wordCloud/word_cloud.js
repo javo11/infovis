7a365ae9da2cf8bc480530a308fcc5810496735a
Javier Fraire <javierfraire92@gmail.com>
2016-03-17 00:01:55 -0700

    changed rotation

--- wordCloud/word_cloud.js
+++ wordCloud/word_cloud.js
@@ -12,11 +12,11 @@ d3.json('word_cloud.json', function (data) {
 });
 
 function wordMap(data) {
-    var fill = d3.scale.category20c();
+    var fill = d3.scale.category20();
     var minFont = Math.random() * (30 - 10) + 10
     var maxFont = Math.random() * (150 - 100) + 100
     var scale = d3.scale.linear().domain([min(data),max(data)]).range([minFont,maxFont]);
-    
+
     d3.select("#words")
         .selectAll()
         .data(data)
@@ -29,9 +29,9 @@ function wordMap(data) {
             return fill(i);
         })
         .style('-webkit-transform', function(d, i) {
-        	return 'rotate(' + ((Math.random() * 4) - 2) + 'deg)'
+        	return 'rotate(' + ((Math.random() * 5) - 2.5) + 'deg)'
         })
-        .style('padding', '2px 5px 2px 5px')
+        .style('padding', '2px 7px 2px 7px')
         .style("font-family", "Courier New, Lucida Console")
         .style("display", "inline-block")
         .html('<b></b>')
