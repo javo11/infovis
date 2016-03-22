8fc5c6ab85f8e233c74f06917fd32c0e6736da8c
Javier Fraire <javierfraire92@gmail.com>
2016-03-21 22:06:20 -0700

    Updated UI on all exercises

--- wordCloud/word_cloud.js
+++ wordCloud/word_cloud.js
@@ -1,6 +1,5 @@
-
 /** Codigo de NicoBuch **/
-d3.json('word_cloud.json', function (data) {
+d3.json('word_cloud.json', function(data) {
     wordMap(data);
 });
 
@@ -8,27 +7,26 @@ function wordMap(data) {
     var fill = d3.scale.category20();
     var minFont = Math.random() * (30 - 10) + 10
     var maxFont = Math.random() * (125 - 100) + 100
-    var scale = d3.scale.linear().domain([min(data),max(data)]).range([minFont,maxFont]);
+    var scale = d3.scale.linear().domain([min(data), max(data)]).range([minFont, maxFont]);
 
     d3.select("#words")
         .selectAll()
         .data(data)
         .enter()
         .append("span")
-        .style("font-size", function (d, i) {
+        .style("font-size", function(d, i) {
             return scale(d.count) + "px";
         })
-        .style("color",function(d, i) {
+        .style("color", function(d, i) {
             return fill(i);
         })
         .style('-webkit-transform', function(d, i) {
-        	return 'rotate(' + ((Math.random() * 5) - 2.5) + 'deg)'
+            return 'rotate(' + ((Math.random() * 5) - 2.5) + 'deg)'
         })
         .style('padding', '2px 7px 2px 7px')
-        .style("font-family", "Courier New, Lucida Console")
         .style("display", "inline-block")
         .html('<b></b>')
-        .text(function (d, i) {
+        .text(function(d, i) {
             return d.word;
         });
 }
